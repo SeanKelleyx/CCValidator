f38415741aaff28e8daa4859ddf29f462d3e732d
Sean Kelley <s.kelley27@gmail.com>
2015-12-28 16:06:26 -0600

    fixed assignment of undefined and validate strings passed as allowedCards

--- ccValidator.js
+++ ccValidator.js
@@ -2,7 +2,7 @@
 	$.fn.validCC = function(options){
 		options = options || {};
 		var cardPrefixesAndLengths = $.parseJSON('{"amex":{"startsWith":["34","37"],"min":15,"max":15},"dinersCart":{"startsWith":["300", "301", "302", "303", "304", "305"],"min":14,"max":14},"dinersInt":{"startsWith":["36"],"min":14,"max":14},"dinersUS":{"startsWith":["54"],"min":16,"max":16},"discover":{"startsWith":["6011", "6221", "6222", "6223", "6224", "6225", "6226", "6227", "6228", "6229", "644", "645", "646", "647", "648", "649", "65"],"min":16,"max":16},"insta":{"startsWith":["637", "638", "639"],"min":16,"max":16},"jcb":{"startsWith":["352","353", "354", "355", "356", "357", "358"],"min":16,"max":16},"laser":{"startsWith":["6304", "6706", "6771", "6709"],"min":16,"max":19},"maestro":{"startsWith":["5018", "5020", "5038", "5893", "6304", "6759", "6761", "6762", "6763"],"min":16,"max":19},"master":{"startsWith":["51", "52", "53", "54", "55"],"min":16,"max":19},"visa":{"startsWith":["4"],"min":13,"max":16},"visae":{"startsWith":["4026", "417500", "4508", "4844", "4913", "4917"],"min":16,"max":16}}');
-		var separator = options.separator || undefined;
+		var separator = options.separator ? "-" : undefined;
 		var allowedCards = options.separator || Object.keys(cardPrefixesAndLengths);
 		var min, max;
 		var prefixes = [];
@@ -51,6 +51,17 @@
 		}
 
 		function setCardParameters(){
+			var allKeys = Object.keys(cardPrefixesAndLengths);
+			var remove = [];
+			for(var i in allowedCards){
+				if (allKeys.indexOf(allowedCards[i]) === -1 ){
+					remove.push(i);
+				}
+			}
+			remove.reverse();
+			for(var i in remove){
+				allowedCards.splice(remove[i], 1);
+			}
 			var mins = [];
 			var maxs = [];
 			for(var i in allowedCards){

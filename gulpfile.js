37db88f1beeed09a9c4fdce131e37cd144312b1b
Sean Kelley <s.kelley27@gmail.com>
2016-01-17 17:58:02 -0600

    add gulpfile and minified version of ccValidator for production use

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	del = require('del');
+
+gulp.task("minifyScript", function(){
+	return gulp.src("ccValidator.js")
+	.pipe(uglify())
+	.pipe(rename("ccValidator.min.js"))
+	.pipe(gulp.dest('./'));
+});
+
+gulp.task('clean', function(){
+	del(['ccValidator.min.js']);
+});
+
+gulp.task("default", ['clean'], function(){
+	gulp.start('minifyScript');
+});

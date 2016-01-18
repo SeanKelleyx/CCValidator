fc4888103e7435c66c21d33c2d515c9c8e46f85e
Sean Kelley <s.kelley27@gmail.com>
2016-01-17 20:27:29 -0600

    adding tests for validLength() and getCardType()

--- test/tests.js
+++ test/tests.js
@@ -27,26 +27,78 @@
 	QUnit.test("test validCC() acceptedCards visa", function(assert){
 		$input.val("4024007160979943");
 		assert.equal(true, $input.validCC({acceptedCards: ["visa"]}));
-		assert.equal(false, $input.validCC({acceptedCards: ["amex","master","maestro","dinersUS"]}));
+		assert.equal(false, $input.validCC({acceptedCards: ["amex","master","maestro","dinersInt"]}));
 	});
 	QUnit.test("test validCC() acceptedCards mastercard", function(assert){
 		$input.val("5109137489501980");
 		assert.equal(true, $input.validCC({acceptedCards: ["master"]}));
-		assert.equal(false, $input.validCC({acceptedCards: ["amex","visa","maestro","dinersUS"]}));
+		assert.equal(false, $input.validCC({acceptedCards: ["amex","visa","maestro","dinersInt"]}));
 	});
 	QUnit.test("test validCC() acceptedCards discover", function(assert){
 		$input.val("6011352767031544");
 		assert.equal(true, $input.validCC({acceptedCards: ["discover"]}));
-		assert.equal(false, $input.validCC({acceptedCards: ["amex","master","maestro","dinersUS","visa"]}));
+		assert.equal(false, $input.validCC({acceptedCards: ["amex","master","maestro","dinersInt","visa"]}));
 	});
 	QUnit.test("test validCC() acceptedCards amex", function(assert){
 		$input.val("347425443456620");
 		assert.equal(true, $input.validCC({acceptedCards: ["amex"]}));
-		assert.equal(false, $input.validCC({acceptedCards: ["visa","master","maestro","dinersUS"]}));
+		assert.equal(false, $input.validCC({acceptedCards: ["visa","master","maestro","dinersInt"]}));
 	});
 	QUnit.test("test validCC() acceptedCards diners", function(assert){
 		$input.val("36951285762086");
 		assert.equal(true, $input.validCC({acceptedCards: ["dinersInt"]}));
 		assert.equal(false, $input.validCC({acceptedCards: ["amex","master","maestro","visa"]}));
+	});
+	QUnit.test("test validCC() on success visa", function( assert){
+		$input.val("4024007160979943");
+		var clickedSuccess = false;
+		$input.validCC({on: "click", success: function(){
+			clickedSuccess = true;
+		}});
+		assert.equal(false, clickedSuccess);
+		$input.trigger("click");
+		assert.equal(true, clickedSuccess);
+	});
+	QUnit.test("test validCC() on failure visa", function( assert){
+		$input.val("12345");
+		var clickedFailure = false;
+		$input.validCC({on: "click", failure: function(){
+			clickedFailure = true;
+		}});
+		assert.equal(false, clickedFailure);
+		$input.trigger("click");
+		assert.equal(true, clickedFailure);
+	});
+	QUnit.test("test validLength() no options", function(assert){
+		$input.val("12345");
+		assert.equal(false, $input.validLength());
+		$input.val("12345678901234");
+		assert.equal(true, $input.validLength());
+	});
+	QUnit.test("test validLength() acceptedCards", function(assert){
+		$input.val("1234567890123");
+		assert.equal(false, $input.validLength({acceptedCards: ["amex"]}));
+		$input.val("123456789012345");
+		assert.equal(true, $input.validLength({acceptedCards: ["amex"]}));
+	});
+	QUnit.test("test getCardType() visa", function(assert){
+		$input.val("4024007160979943");
+		assert.equal("visa", $input.getCardType());
+	});
+	QUnit.test("test getCardType() mastercard", function(assert){
+		$input.val("5109137489501980");
+		assert.equal("master", $input.getCardType());
+	});
+	QUnit.test("test getCardType() discover", function(assert){
+		$input.val("6011352767031544");
+		assert.equal("discover", $input.getCardType());
+	});
+	QUnit.test("test getCardType() amex", function(assert){
+		$input.val("347425443456620");
+		assert.equal("amex", $input.getCardType());
+	});
+	QUnit.test("test getCardType() diners", function(assert){
+		$input.val("36951285762086");
+		assert.equal("dinersInt", $input.getCardType());
 	});
 })(jQuery);
